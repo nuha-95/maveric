2fb3d22064297c7b383c8cf4aa2433ad46405a5f
Nusrat Jahan Nuha <nusratjahannuha95@gmail.com>
2025-09-23 15:11:26 +0600

    updated the inference

--- apps/mobility_robustness_optimization/modular_rl_mro/rl_inference.py
+++ apps/mobility_robustness_optimization/modular_rl_mro/rl_inference.py
@@ -2,6 +2,7 @@ import logging
 import pandas as pd
 import sys
 import os
+import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 
@@ -9,32 +10,35 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 maveric_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 sys.path.insert(0, maveric_root)
 
-from mro_utils import calculate_mro_metric
-
-from radp.digital_twin.utils.cell_selection import perform_attachment_hyst_ttt, find_hyst_diff
+from radp.digital_twin.utils.cell_selection import find_hyst_diff
 from radp.digital_twin.utils.constants import RLF_THRESHOLD
-from rl_environment import ReinforcedMROEnv
+from rl_utils import ReinforcedMROEnv
 
 logger = logging.getLogger(__name__)
 
 
-def rl_mro_inference(ppo_model):
-    """Get optimal parameters from trained PPO model."""
+def rl_mro_inference(simulation_data, ppo_model):
+    
     
     print("Loading trained PPO model for parameter prediction...")
     
     # Load trained PPO model
     ppo_model = PPO.load(ppo_model)
     
-    # Create dummy environment for prediction (parameters will be extracted from model)
+    # Define parameter ranges from simulation data 
+    max_diff = find_hyst_diff(simulation_data)
+    num_ticks = simulation_data["tick"].nunique()
+    hyst_range = [0, max_diff]
+    ttt_range = [2, num_ticks + 1]
     
-    dummy_env = DummyVecEnv([lambda: ReinforcedMROEnv(None, RLF_THRESHOLD, [0, 1], [2, 50])])
+    # Create dummy environment 
+    dummy_env = DummyVecEnv([lambda: ReinforcedMROEnv(simulation_data, RLF_THRESHOLD, hyst_range, ttt_range)])
     
-    # Predict optimal action using trained PPO agent
+    # Predict optimal action using trained model
     obs = dummy_env.reset()
     action, _ = ppo_model.predict(obs, deterministic=True)
-
-    # Extract parameters
+    
+    # Ensure ttt is an integer
     hyst, ttt = action[0]
     ttt = int(round(ttt))
     
@@ -44,5 +48,3 @@ def rl_mro_inference(ppo_model):
         'hysteresis': hyst,
         'ttt': ttt
     }
-
-
